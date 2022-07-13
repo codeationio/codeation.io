b24fac309238a3e6636bfa5480bda791251c6178
Anoop poonia <apoonia030@gmail.com>
2022-07-13 13:32:15 +0530

    feat: assets

--- components/Avatar.tsx
+++ components/Avatar.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react';
 
-const Avatar: FC = ({ children }) => {
+type Props ={
+  children:any
+}
+
+const Avatar: FC<Props> = ({ children }) => {
   return (
     <div className="rounded-full h-24 w-24 flex items-center justify-center m-4 border-dashed border bg-blue-400">
       {children}
