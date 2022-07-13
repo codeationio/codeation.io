49b79271e1294de110957424d5a639a3f8d3baa6
Anoop poonia <apoonia030@gmail.com>
2022-07-13 15:31:13 +0530

    tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,12 @@ module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   plugins: [],
   theme: {
-    extend: {},
     colors: {
-      primary: '#3E8BFF',
-      textColor: '#4D5F7A',
       description: '#8896AB',
-      bgColor: '#F8F8F8'
-    }
+      'light-grey': '#F8F8F8',
+      primary: '#3E8BFF',
+      textColor: '#4D5F7A'
+    },
+    extend: {}
   }
 };
