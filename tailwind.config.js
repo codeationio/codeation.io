2bf46053eae7a4bc0177d7732b956da010bb325d
Rishabh Jain <hello@rishabhjain.dev>
2022-07-13 16:47:00 +0530

    feat: fixed font and added card changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,30 @@ module.exports = {
       description: '#8896AB',
       'light-grey': '#F8F8F8',
       primary: '#3E8BFF',
-      textColor: '#4D5F7A'
+      textColor: '#4D5F7A',
+      white: '#FFFFFF'
     },
-    extend: {}
+    extend: {
+      boxShadow: {
+        card: '0px 3.37869px 6.75738px rgba(15, 18, 33, 0.1)'
+      },
+      fontSize: {
+        'card-description': [
+          '1rem',
+          {
+            fontWeight: 400,
+            lineHeight: '1.25rem'
+          }
+        ],
+        'card-title': [
+          '1.625rem',
+          {
+            fontWeight: 700,
+            lineHeight: '2rem'
+          }
+        ]
+      }
+    },
+    fontFamily: ['Montserrat', 'sans-serif']
   }
 };
