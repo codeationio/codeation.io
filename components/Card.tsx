5c6976cab2d887b78a4b54e2c9de74fb66e07364
Rishabh Jain <hello@rishabhjain.dev>
2022-07-14 10:53:54 +0530

    feat: Card and Service card component

---
+++ components/Card.tsx
@@ -0,0 +1,43 @@
+import { FCC } from 'globalTypes';
+import Image from 'next/image';
+
+export type CardProps = {
+  className?: string;
+};
+
+const Card: FCC<CardProps> = ({ children, className }) => {
+  return <article className={`rounded-lg shadow-card ${className}`}>{children}</article>;
+};
+
+Card.defaultProps = {
+  className: ''
+};
+
+export const CardTitle: FCC = ({ children }) => {
+  return (
+    <h4 className="mt-4 mb-3 text-center text-card-title capitalize text-primary">{children}</h4>
+  );
+};
+
+export const CardDescription: FCC = ({ children }) => {
+  return (
+    <p className="max-w-[250px] text-center text-card-description text-description">{children}</p>
+  );
+};
+
+export type ServicesCardProps = {
+  title: string;
+  description: string;
+  image: string;
+};
+export const ServicesCard: FCC<ServicesCardProps> = ({ description, image, title }) => {
+  return (
+    <Card className="flex min-w-fit flex-col items-center justify-center bg-white px-14 pt-10 pb-14">
+      <Image alt="Card" height={209} objectFit="fill" src={image} width={236} />
+      <CardTitle>{title}</CardTitle>
+      <CardDescription>{description}</CardDescription>
+    </Card>
+  );
+};
+
+export default Card;
