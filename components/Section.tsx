c366001fd5a2d75a30904a7e6fc73cf3a17fc638
Rishabh Jain <hello@rishabhjain.dev>
2022-07-14 12:22:17 +0530

    feat: added Section and wrapper

--- components/Section.tsx
+++ components/Section.tsx
@@ -3,13 +3,14 @@ import { ReactNode } from 'react';
 
 export type SectionProps = {
   className?: string;
-  title?: ReactNode;
   head?: boolean;
+  id?: string;
+  title?: ReactNode;
 };
 
-const Section: FCC<SectionProps> = ({ children, className, head, title }) => {
+const Section: FCC<SectionProps> = ({ children, className, head, id, title }) => {
   return (
-    <section className={head ? '' : 'my-12'}>
+    <section className={head ? '' : 'my-12'} id={id}>
       {title && (
         <header>
           <h1 className="relative mb-12 text-center text-5xl font-bold text-primary before:absolute before:-bottom-4 before:left-1/2 before:w-40 before:-translate-x-1/2 before:border-b-2 before:content-['_']">
--- components/Wrapper.tsx
+++ components/Wrapper.tsx
@@ -5,7 +5,9 @@ const Wrapper: FCC = ({ children }) => {
   return (
     <div className="bg-black [&>*:nth-child(odd)]:bg-white [&>*:nth-child(even)]:bg-light-grey">
       {React.Children.map(children, (child) => (
-        <div className="[&>*]:max-w-8xl flex w-screen flex-col items-center">{child}</div>
+        <div className="flex w-screen snap-y snap-mandatory flex-col items-center [&>*]:w-full [&>*]:max-w-8xl [&>*]:snap-start [&>*]:snap-always">
+          {child}
+        </div>
       ))}
     </div>
   );
