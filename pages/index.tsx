c365b119a8dec488f91b6da94b8bcf3b83c74fa2
Rishabh Jain <hello@rishabhjain.dev>
2022-07-14 12:23:20 +0530

    feat: added Our services section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,41 @@
+import { ServicesCard } from 'components/Card';
 import Navbar from 'components/Navbar';
+import Section from 'components/Section';
 import Head from 'next/head';
 import { FC } from 'react';
 
 import Hero from '../components/Hero';
 
-const index: FC = () => {
+const cards: any[] = Array.from({ length: 9 }).fill({
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipcing elit. Sit malesuada massa velit gravida hendrerit venenatis phasellus.',
+  image: '/images/web_development.svg',
+  title: 'web development'
+});
+
+const Index: FC = () => {
   return (
-    <div className="h-screen bg-light-grey">
+    <>
       <Head>
         <title>Codeation.io next-js template</title>
         <link href="/favicon.ico" rel="icon" />
       </Head>
-      <Navbar />
-      <Hero />
-    </div>
+      <Section head>
+        <Navbar />
+        <Hero />
+      </Section>
+      <Section className="flex flex-wrap justify-between gap-8" title="Our Services">
+        {cards.map((card, index) => (
+          <ServicesCard
+            key={index}
+            description={card.description}
+            image={card.image}
+            title={card.title}
+          />
+        ))}
+      </Section>
+    </>
   );
 };
 
-export default index;
+export default Index;
