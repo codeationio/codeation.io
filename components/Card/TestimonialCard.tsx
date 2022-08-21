e2cc2bf72a3508d53dc03f51befd2f992ca85543
Rishabh Jain <hello@rishabhjain.dev>
2022-08-21 23:22:53 +0530

    fix: image in testimonials

--- components/Card/TestimonialCard.tsx
+++ components/Card/TestimonialCard.tsx
@@ -19,14 +19,13 @@ const TestimonialCard: FCC<TestimonialProps> = ({ company, description, designat
     <Card
       className="carousel-item flex min-w-fit scroll-m-16 flex-col items-center justify-center gap-y-1 bg-white p-4 pt-0 lg:w-80 xl:w-80 xl:min-w-fit xl:pb-6 [&>span]:!overflow-visible"
       id={id}>
-      {/* <Image
+      <Image
         alt={`${name}${company ? ` | ${company}` : ''}`}
-        className="-translate-y-16 rounded-full object-fill"
-        height={42}
+        className="h-24 w-24 -translate-y-16 rounded-full object-fill lg:h-32 lg:w-32"
+        height={96}
         src={url}
         width={132}
-      /> */}
-      <img alt={`${name}${company ? ` | ${company}` : ''}`} className="h-24 -translate-y-16 rounded-full object-fill lg:h-32" src={url} />
+      />
       <Image alt="comma" className="-mt-8 -translate-y-5 lg:-translate-y-2" height={25} src="/images/comma.svg" width={32} />
       <CardTitle className="-mt-3 mb-0 text-xl text-black lg:text-[28px]">{name}</CardTitle>
       <p className="my-2 h-5 text-xs font-medium text-description lg:mt-0">
