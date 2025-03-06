c28f8a3fc9c0637e03b75e45c5846d4f2338995b
Rishabh Jain <hello@rishabhjain.dev>
2025-03-07 02:48:04 +0530

    chore: added fb verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="scroll-smooth" lang="en">
-      <Head />
+      <Head>
+        <meta name="facebook-domain-verification" content="27x0m611oyiye9qxfbbf3gza53w09g" />
+      </Head>
       <body className="dark">
         <Main />
         <NextScript />
